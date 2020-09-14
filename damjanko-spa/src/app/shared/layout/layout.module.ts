e29b6a2dd3f6beb62003ea07887d01cb7979328e
Damjan Kovacic <letecnez@gmail.com>
2020-09-14 20:24:14 +0200

    Layout (shared) module

---
+++ damjanko-spa/src/app/shared/layout/layout.module.ts
@@ -0,0 +1,11 @@
+/* 3rd party */
+import { NgModule } from '@angular/core';
+
+/* 1st party */
+import { CommonImportsModule } from '@core/common-imports.module';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonImportsModule],
+})
+export class LayoutModule {}

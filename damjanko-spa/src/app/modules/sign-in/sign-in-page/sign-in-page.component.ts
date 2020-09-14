3e64a356a34b30eaaf091fecaf4da4e8a994b21d
Damjan Kovacic <letecnez@gmail.com>
2020-09-14 19:17:21 +0200

    Changed component prefix from "app" to "dko"

--- damjanko-spa/src/app/modules/sign-in/sign-in-page/sign-in-page.component.ts
+++ damjanko-spa/src/app/modules/sign-in/sign-in-page/sign-in-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-sign-in-page',
+    selector: 'dko-sign-in-page',
     templateUrl: './sign-in-page.component.html',
     styleUrls: ['./sign-in-page.component.scss'],
 })

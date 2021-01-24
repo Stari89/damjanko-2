25eddc3200b424a242fd9e0eda17f354c7a1cabc
Damjan Kovacic <letecnez@gmail.com>
2021-01-24 20:14:09 +0100

    Backoffice app

---
+++ damjanko-back-office/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Typography from '@material-ui/core/Typography';
+
+function App() {
+    return <Typography component="p">DamjanKo.com backoffice</Typography>;
+}
+
+export default App;

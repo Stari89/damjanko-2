2cc9b44df04ced73a5353956a837dc932af480ca
Damjan Kovacic <letecnez@gmail.com>
2021-01-24 19:58:48 +0100

    Installed MaterialUI

--- damjanko-spa/src/App.tsx
+++ damjanko-spa/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
-    return <p>DamjanKo.com homepage</p>;
+    return <Typography component="p">DamjanKo.com homepage</Typography>;
 }
 
 export default App;
